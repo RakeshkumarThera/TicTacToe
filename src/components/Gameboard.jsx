4c4ac504e28a5eb689cb09d390fc663e65e61f4a
Rakesh kumar Thera <rakeshkumarthera@rakeshs-mbp.home>
2024-02-28 21:28:13 -0500

    final update

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,18 +1,48 @@
-const initalGameboard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-]
-export default function Gameboard(){
+import { useState } from "react"
+
+// const initalGameboard = [
+//     [null, null, null],
+//     [null, null, null],
+//     [null, null, null]
+// ]
+
+export default function Gameboard({onSelectSquare, board}){
+
+    // let gameBoard = initalGameboard;
+
+    // for(const turn of turns){
+    //     const {square, player} = turn;
+    //     const {row, col} = square;
+
+    //     gameBoard[row][col] = player;
+    // }
+
+    // const[gameBoard, setGameBoard] = useState(initalGameboard);
+
+    // function handleSelectedSquare(colIndex, rowIndex){
+    //     setGameBoard((prevGameBoard)=> {
+    //         const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])]; //Best Practice of updating object state Immutably ref: https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/39659798#questions
+    //         updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //     return updatedGameBoard;
+    //     });
+
+    //     onSelectSquare();
+    // }
+
     return(
        <ol id='game-board'>
         {
-            initalGameboard.map((row, rowIndex) => <li key={rowIndex}>
+            board.map((row, rowIndex) => ( 
+            <li key={rowIndex}>
                 <ol>
-                    {row.map((col, colIndex) => <li key={colIndex}><button>{col}</button></li>)}
+                    {row.map((playerSymbol, colIndex) => (
+                    <li key={colIndex}>
+                        <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol}>{playerSymbol}</button>
+                    </li>
+                    ))}
                 </ol>
-            </li>)
-        }
+            </li>
+            ))}
        </ol>
     )
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react';
-export default function Player({initialName, symbol}){
+export default function Player({initialName, symbol, isActive, onChangeName}){
     const [playerName, setPlayername] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick(){
         setIsEditing(editing => !editing) //this is recommended apporach
         // setIsEditing(!setIsEditing) //this is  not recommended apporach
-
+        
+        if(isEditing){
+            onChangeName(symbol, playerName);
+        }
     }
 
     function handleChange(event){
@@ -14,7 +17,7 @@ export default function Player({initialName, symbol}){
     }
 
     return(
-        <li>
+        <li className={isActive?'active':undefined}>
         <span id="player">
           {isEditing ? (<input className="player-input" type="text" required value={playerName} onChange={handleChange}/>) //Changing value and feeding back the value is called as two way binding
           : (<span className="player-name">{playerName}</span>)}
